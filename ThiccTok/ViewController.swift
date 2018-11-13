f4b54bc9d58171b02ce76486a7864be9c3f1572c
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2018-11-13 11:12:35 -0600

    Initial Commit

---
+++ ThiccTok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThiccTok
+//
+//  Created by Patrick Dowell on 11/13/18.
+//  Copyright © 2018 Patrick Dowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
